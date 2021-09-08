7717c51c1261e1b0e883817c68eeef1652fea48e
Kamil Biczyk <kamilbiczyk@Air-de-Urszula.home>
2021-09-09 00:53:59 +0200

    Update

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: kamilbiczyk <kamilbiczyk@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/08 09:38:54 by kamilbiczyk       #+#    #+#             */
-/*   Updated: 2021/09/08 14:05:34 by kamilbiczyk      ###   ########.fr       */
+/*   Updated: 2021/09/09 00:48:36 by kamilbiczyk      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include "get_next_line.h"
+
 /*
 ** This function appends a single line into our line variable. We do this by
 ** finding the length of the line. If at index len, the character is a newline,
@@ -26,23 +27,65 @@
 ** At any point when we reach the end of the file, we free the memory used to
 ** track our location in *s because it is not needed anymore.
 */
-char *get_next_line(int fd)
+
+char    *get_line(char *s, int line)
 {
+    int current_line;
+    int i;
+
+    i = 0;
+    current_line = 0;
+    while(*s)
+    {
+        if (current_line == line)
+            break;
+        if (*s == '\n')
+            current_line++;
+        s++;
+    }
+    while(s[i] != '\n' && s[i] != '\0')
+    {
+        printf("%c", s[i]);
+        i++;
+    }
+    printf("\n");
+    return (0);
+}
+
+char    *get_next_line(int fd)
+{
+    static int  x = 0;
     char    c;
     char    *buf;
 
+    if (fd == -1)
+    {
+        printf("Fichier passé en param pas bon.\n");
+        return (0);
+    }
     buf = malloc(BUFF_SIZE + 1);
     if (!buf)
         return (NULL);
     c = read(fd, buf, BUFF_SIZE);
-    printf("%d", c);
+    buf[c] = '\0';
+    get_line(buf, x);
+    x++;
     return (0);
+}
+
+int main_open_file(char *c)
+{
+    int fd;
+
+    fd = open(c, O_RDONLY);
+    return (fd);
 }
 
 int main(void)
 {
-    int fd;
-    
-    fd = open("testfile", O_RDONLY);
-    printf("%s", get_next_line(fd));
+    get_next_line(main_open_file("testfile"));
+    get_next_line(main_open_file("testfile"));
+    get_next_line(main_open_file("testfile"));
+    get_next_line(main_open_file("testfile"));
+
 }
